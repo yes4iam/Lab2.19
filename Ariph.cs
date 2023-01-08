78cb658bbe75d0198f329c26a9ea5011ccd9e3d7
Ilya Markov <imarkovkin@gmail.com>
2023-01-09 02:27:28 +0300

    Добавьте файлы проекта.

---
+++ Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._19
+{
+    static public class Ariph
+    {
+        static public int Add(int a) => 2*Convert.ToInt32(Math.PI)*a;
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab2._19.ViewModels
+{
+    class MainWindowViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }       
+
+        private int circleLength;
+        public int CircleLength
+        {
+            get => circleLength;
+            set
+            {
+                circleLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand Addcommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            CircleLength = Ariph.Add(radius);
+        }
+        private bool CanAddCommandExecuted (object p)
+        {
+            if (radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            Addcommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
